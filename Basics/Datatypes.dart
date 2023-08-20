6130461ff41400bd615622473ccae678d775a142
Prashik Wankhade <111126326+PrashikWankhadeSCOE@users.noreply.github.com>
2023-08-21 00:14:47 +0530

    Add files via upload

---
+++ Basics/Datatypes.dart
@@ -0,0 +1,24 @@
+/*
+void main() {
+  print("Prashik");
+  print("Wankhade");
+
+//  int x;
+// print(x);       // Non nullabe variable must be assigned before it can be used
+
+  int x = 10;
+  int y = 20;
+  print(x);
+  print(y);
+}
+*/
+
+void main() {
+  int x = 10;
+  print(x);
+
+//  x = 20.5;       // A value of type "double" can not be assignied to variable of type "int"
+
+  num y = 30.43;
+  print(y);
+}
