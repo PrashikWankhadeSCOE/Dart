eef5213ed50b3471787c3f960031e6fdf45152be
Prashik Wankhade <111126326+PrashikWankhadeSCOE@users.noreply.github.com>
2023-08-24 14:33:07 +0530

    Add files via upload

---
+++ Excercise/if-else-statement_Assignment/Program10.dart
@@ -0,0 +1,22 @@
+void main() {
+  var num = 90;
+  if (num >= 90 && num < 180) {
+    print(num * 6);
+  } else if (num >= 180 && num < 250) {
+    print(num * 10);
+  } else if (num >= 250) {
+    print(num * 15);
+  } else {
+    print("No charges");
+  }
+  num = 120;
+  if (num >= 90 && num < 180) {
+    print(num * 6);
+  } else if (num >= 180 && num < 250) {
+    print(num * 10);
+  } else if (num >= 250) {
+    print(num * 15);
+  } else {
+    print("No charges");
+  }
+}
---
+++ Excercise/if-else-statement_Assignment/Program9.dart
@@ -0,0 +1,26 @@
+void main() {
+  print("For Upper Stand Ticket -- 1 -- price == 2000");
+  print("For Middle Stand Ticket -- 2 -- price == 3000");
+  print("For Lower Stand Ticket -- 3 -- price == 7000");
+  print("All other -- price == 2500");
+  var num = 1;
+  if (num == 1) {
+    print("Plese pay 2000");
+  } else if (num == 2) {
+    print("please pay 3000");
+  } else if (num == 3) {
+    print("Please Pay 7500");
+  } else {
+    print("Please Pay 2500");
+  }
+  num = 6;
+  if (num == 1) {
+    print("Plese pay 2000");
+  } else if (num == 2) {
+    print("please pay 3000");
+  } else if (num == 3) {
+    print("Please Pay 7500");
+  } else {
+    print("Please Pay 2500");
+  }
+}
