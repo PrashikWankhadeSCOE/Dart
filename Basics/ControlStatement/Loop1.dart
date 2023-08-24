d1d3cda01e7de20c0f85899fe8148cb5691d2e0d
Prashik Wankhade <111126326+PrashikWankhadeSCOE@users.noreply.github.com>
2023-08-24 11:59:37 +0530

    Add files via upload

---
+++ Basics/ControlStatement/Loop1.dart
@@ -0,0 +1,12 @@
+void main() {
+  for (int i = 0; i < 50; i++) {
+    if (i % 5 == 0 && i % 4 == 0) {
+      print(i);
+    }
+  }
+  int x = 1;
+  while (x <= 10) {
+    print(x);
+    x++;
+  }
+}
--- Basics/ControlStatement/Program2.dart
+++ Basics/ControlStatement/Program2.dart
@@ -1,1 +1,16 @@
+// If- Else Ladder
 
+void main() {
+  int marks = 81;
+  if (marks >= 90) {
+    print("Gold medal from university");
+  } else if (marks >= 80 && marks < 90) {
+    print("Grade A -- First Class with Distinction");
+  } else if (marks >= 65 && marks < 80) {
+    print("Grade B -- First Class");
+  } else if (marks >= 40 && marks < 65) {
+    print("Pass");
+  } else {
+    print("Fail");
+  }
+}
