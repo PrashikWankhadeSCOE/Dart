f5938fca5b0537ef0086fb0a606f86491231cca2
Prashik Wankhade <111126326+PrashikWankhadeSCOE@users.noreply.github.com>
2023-09-13 15:01:23 +0530

    Add files via upload

---
+++ Basics/LambdaFunction/Program3.dart
@@ -0,0 +1,6 @@
+var add = (int x, int y) {
+  return x + y;
+};
+void main() {
+  print(add(10, 20));
+}
---
+++ Basics/LambdaFunction/Program6.dart
@@ -0,0 +1,4 @@
+void main() {
+  var PlayerData = () => print("In PlayerData");
+  PlayerData();
+}
